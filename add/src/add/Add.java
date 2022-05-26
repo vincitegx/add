38764f5ac06346959ceee195483223eb1a82287c
David Tega <40434274+vincitegx@users.noreply.github.com>
2022-05-26 02:14:41 +0100

    Add files via upload

---
+++ add/src/add/Add.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package add;
+
+/**
+ *
+ * @author Eee
+ */
+public class Add {
+    public static void main(String[] args){
+        System.out.println((10.5 + 2 * 3)/(45-3.5));
+    }
+        
+     
+
+    /**
+     * @param args the command line arguments
+     */
+        // TODO code application logic here
+    
+    
+}
